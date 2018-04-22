cbb20feca7b51e26ea6d373f7cdb4783995283ab
corgitronics <corgitronics.com@gmail.com>
2018-04-21 21:03:19 -0500

    communications working to blink LED

---
+++ arm-1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+from pyax12.connection import Connection
+import time
+import RPi.GPIO as GPIO
+import pyax12.packet as pk
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(18, GPIO.OUT)
+
+port = '/dev/ttyS0'
+baudrate = 1000000
+timeout = 3
+tx_rx = 18
+
+dynamixel_id = 3
+
+serial_connection = Connection(port=port,
+                                   baudrate=baudrate,
+                                   timeout=timeout,
+                                   rpi_gpio=tx_rx)
+
+# Switch ON the LED
+serial_connection.write_data(dynamixel_id, pk.LED, 1)
+
+# Wait 2 seconds
+time.sleep(2)
+
+# Switch OFF the LED
+serial_connection.write_data(dynamixel_id, pk.LED, 0)
+
+# Wait 2 seconds
+time.sleep(2)
+
+# Switch ON the LED
+serial_connection.write_data(dynamixel_id, pk.LED, 1)
+
+# Wait 2 seconds
+time.sleep(2)
+
+# Switch OFF the LED
+serial_connection.write_data(dynamixel_id, pk.LED, 0)
+
+# Close the serial connection
+serial_connection.close()
